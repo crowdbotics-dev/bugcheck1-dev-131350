a8416abf0e2541c2c40e3d35b2e3d07e827ca019
Crowdbotics <engineering@crowdbotics.com>
2023-12-06 14:33:40 +0000

    Initial commit

---
+++ backend/bugcheck1_dev_131350/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for bugcheck1_dev_131350 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "bugcheck1_dev_131350.settings"
+)
+
+application = get_wsgi_application()
